ae5496a9276d00fb70275ae5bfdce52eea842815
denkiri <denniskirimi.dk@gmail.com>
2024-07-10 16:43:52 +0300

    Payment Confirmation UI

---
+++ TafaParentApp/models/InvoiceStatus.swift
@@ -0,0 +1,8 @@
+//
+//  InvoiceStatus.swift
+//  TafaParentApp
+//
+//  Created by Macbook Pro on 09/07/2024.
+//
+
+import Foundation
---
+++ TafaParentApp/models/TopUpTokens.swift
@@ -0,0 +1,8 @@
+//
+//  TopUpTokens.swift
+//  TafaParentApp
+//
+//  Created by Macbook Pro on 09/07/2024.
+//
+
+import Foundation
